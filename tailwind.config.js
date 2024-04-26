6df96f7d63f0079a656c55530cf2b9da001dba4f
yoyov5123 <yoyov51234@gmail.com>
2024-04-26 21:41:11 +0800

    profile project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: ["./src/*.{html,js}",
     "./*.{html,js}",
-    "./problems/*.{html,js}"],
+    "./problems/*.{html,js}",
+    "./project_profile/*.{html,js}"],
   theme: {
     extend: {
       fontSize: {
@@ -10,13 +11,21 @@ module.exports = {
       },
       colors: {
         primary: 'hsl(47, 88%, 63%)',
+        profile_project_primary: 'hsl(75, 94%, 57%)',
+        profile_project_white: 'hsl(0, 0%, 100%)',
+        profile_project_Grey: 'hsl(0, 0%, 20%)',
+        profile_project_Dark_Grey: 'hsl(0, 0%, 12%)',
+        profile_project_Off_Black: 'hsl(0, 0%, 8%)'
       },
       fontFamily: {
-        body: ['Figtree']
+        body: ['Figtree'],
+        customFont: ['Inter']
+        // Inter: []
       },
       screens: {
         'mobile320': '320px',
         'tablet': '384px',
+        // "profile384": '384px'
       }
     },
   },
