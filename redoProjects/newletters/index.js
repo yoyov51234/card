d0bbef8f7a79780becd59c2ad8e8a0b6409a7550
yoyov5123 <yoyov51234@gmail.com>
2024-06-06 07:05:09 +0800

    redo newsletters

---
+++ redoProjects/newletters/index.js
@@ -0,0 +1,75 @@
+const submitBtn = document.getElementById('btn-subscirbe');
+const box = document.getElementById('box');
+const box2 = document.getElementById('box2');
+const dismissBtn = document.getElementById('dismiss-btn');
+const btn = document.querySelectorAll('btn');
+const email = document.getElementById('input-email');
+const labelInvalid = document.getElementById('label-error-message');
+// const emailPlacement = document.getElementById('subscirbe__desc');
+const emailPlacement = document.querySelector('.subscirbe__desc strong');
+
+// validate the email input
+email.addEventListener('input', (e) => {
+  if (!email.validity.valid) {
+    console.log('not good');
+    labelInvalid.classList.remove('hide');
+    labelInvalid.classList.add('label__invalid');
+    labelInvalid.classList.add('label__emails');
+    email.classList.add('invalid');
+
+  } else {
+    labelInvalid.classList.remove('label__invalid');
+    labelInvalid.classList.remove('label__emails');
+    labelInvalid.classList.add('hide');
+    email.classList.remove('invalid');
+  }
+
+})
+
+
+// for(item in btn){
+//   item.addEventListener('hover')
+// }
+
+submitBtn.addEventListener('click', (e) => {
+  if (!email.validity.valid) {
+    console.log('not good');
+    labelInvalid.classList.remove('hide');
+    labelInvalid.classList.add('label__invalid');
+    labelInvalid.classList.add('label__emails');
+    email.classList.add('invalid');
+    e.preventDefault();
+
+  } else {
+    labelInvalid.classList.remove('label__invalid');
+    labelInvalid.classList.remove('label__emails');
+    labelInvalid.classList.add('hide');
+    email.classList.add('remove');
+    toggleOnOff(box);
+    toggleOnOff(box2);
+    emailPlacement.textContent = email.value;
+    email.value = '';
+    e.preventDefault();
+  }
+
+
+
+
+})
+
+dismissBtn.addEventListener('click', (e) => {
+  toggleOnOff(box);
+  toggleOnOff(box2);
+  e.preventDefault();
+
+
+})
+
+
+function toggleOnOff(eventTarget) {
+  if (eventTarget.classList.contains('hide')) {
+    eventTarget.classList.remove('hide');
+  } else {
+    eventTarget.classList.add('hide');
+  }
+}
