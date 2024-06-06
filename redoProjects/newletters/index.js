349e7bafce08bc2e7c69b6727635d67d019385b4
yoyov5123 <yoyov51234@gmail.com>
2024-06-06 17:27:32 +0800

    get data using FormData

--- redoProjects/newletters/index.js
+++ redoProjects/newletters/index.js
@@ -7,6 +7,7 @@ const email = document.getElementById('input-email');
 const labelInvalid = document.getElementById('label-error-message');
 // const emailPlacement = document.getElementById('subscirbe__desc');
 const emailPlacement = document.querySelector('.subscirbe__desc strong');
+const form = document.getElementById('form');
 
 // validate the email input
 email.addEventListener('input', (e) => {
@@ -26,36 +27,47 @@ email.addEventListener('input', (e) => {
 
 })
 
+form.addEventListener('submit', (e) => {
 
-// for(item in btn){
-//   item.addEventListener('hover')
-// }
+  e.preventDefault();
+  const formDate = new FormData(form);
+  console.log(typeof (formDate));
+  console.log(formDate);
+  // formDate.get('email')
+  const data = Object.fromEntries(formDate);
+  console.log(typeof (data))
 
-submitBtn.addEventListener('click', (e) => {
-  if (!email.validity.valid) {
-    console.log('not good');
-    labelInvalid.classList.remove('hide');
-    labelInvalid.classList.add('label__invalid');
-    labelInvalid.classList.add('label__emails');
-    email.classList.add('invalid');
-    e.preventDefault();
-
-  } else {
-    labelInvalid.classList.remove('label__invalid');
-    labelInvalid.classList.remove('label__emails');
-    labelInvalid.classList.add('hide');
-    email.classList.add('remove');
-    toggleOnOff(box);
-    toggleOnOff(box2);
-    emailPlacement.textContent = email.value;
-    email.value = '';
-    e.preventDefault();
-  }
-
-
-
+  console.log(data);
 
 })
+
+
+
+// submitBtn.addEventListener('click', (e) => {
+//   if (!email.validity.valid) {
+//     console.log('not good');
+//     labelInvalid.classList.remove('hide');
+//     labelInvalid.classList.add('label__invalid');
+//     labelInvalid.classList.add('label__emails');
+//     email.classList.add('invalid');
+//     e.preventDefault();
+
+//   } else {
+//     labelInvalid.classList.remove('label__invalid');
+//     labelInvalid.classList.remove('label__emails');
+//     labelInvalid.classList.add('hide');
+//     email.classList.add('remove');
+//     toggleOnOff(box);
+//     toggleOnOff(box2);
+//     emailPlacement.textContent = email.value;
+//     email.value = '';
+//     e.preventDefault();
+//   }
+
+
+
+
+// })
 
 dismissBtn.addEventListener('click', (e) => {
   toggleOnOff(box);
